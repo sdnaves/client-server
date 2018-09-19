ca126f564d0abea13bcf7e19109c21bb312933ad
Silas Diniz.Naves <silas.naves@SIRSAN.COM.BR>
2018-09-19 18:51:08 -0300

    Projeto Client Server (Chat)

---
+++ ClientServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args.Contains("teste"))
+                {
+                    Teste();
+                }
+                else if (args.Contains("help"))
+                {
+                    Help();
+                }
+                else
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+        }
+
+        public static void Teste()
+        {
+            Console.WriteLine("Deu Certo!");
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine("Não Implementado Ainda!");
+        }
+    }
+}
---
+++ ClientServer/server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ClientServer
+{
+    public class Server
+    {
+        private Socket socket;
+        private TcpClient client;
+        private TcpListener listener;
+        private static Dictionary<string, TcpClient> dicClient = new Dictionary<string, TcpClient>();
+
+        public int cont = 0;
+        public string readOut;
+        public static string readData;
+        public static bool dataBru = false;
+
+        bool socketRunnig = false;
+
+        List<TcpClient> listClient = new List<TcpClient>();
+
+        public Server() { }
+        public Server(string serverIp)
+        {
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach (IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    serverIp = address.ToString();
+                }
+            }
+        }
+
+        public void Start(string serverPort)
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+    }
+}
